e1cda8160fc2e55101ff1b477955492725aa7576
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-09-21 03:08:05 -0400

    Switched to using premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Shockwave"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	startproject "Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SW_PLATFORM_WINDOWS",
+			"SW_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "SW_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SW_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SW_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SW_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SW_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SW_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SW_DIST"
+		optimize "On"
